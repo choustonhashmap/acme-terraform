a4ed9434e14bbd9a7f98d4c6c2d449532d3d9308
choustonhashmap <chris.houston@hashmapinc.com>
2020-03-04 18:18:28 -0500

    another one

--- main.tf
+++ main.tf
@@ -13,3 +13,14 @@ provider "aws" {
 alias = "euc1"
 region = "eu-central-1"
 }
+
+resource "aws_elastic_beanstalk_application" "tftest" {
+  name        = "tf-test-name"
+  description = "tf-test-desc"
+}
+
+resource "aws_elastic_beanstalk_environment" "tfenvtest" {
+  name                = "tf-test-name"
+  application         = "${aws_elastic_beanstalk_application.tftest.name}"
+  solution_stack_name = "apache"
+}
