a775a5d665fc2966ef15597f278b1400e77076df
choustonhashmap <chris.houston@hashmapinc.com>
2020-03-04 20:11:37 -0500

    Added proper output

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ data "terraform_remote_state" "vpc_list"{
 }
 
 output "vpc"{
-  value = "${data.terraform_remote_state.vpc_list.app-vpc-eu-central-1.vpc_id}"
+  value = "${data.terraform_remote_state.vpc_list.outputs.admin-vpc-usw1.value}"
 }
 
 provider "aws"{
