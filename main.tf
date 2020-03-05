1785e67fd34236ec723c5b8a2d03f1ca570be0be
choustonhashmap <chris.houston@hashmapinc.com>
2020-03-05 12:11:32 -0500

    security group removed for bug test

--- main.tf
+++ main.tf
@@ -18,16 +18,17 @@ provider "aws" {
   alias = "usw2"
   region     = "us-west-2"
 }
-# provider "aws" {
-#   alias = "usw1"
-#   region = "us-west-1"
-# }
+provider "aws" {
+  alias = "usw1"
+  region = "us-west-1"
+}
 provider "aws" {
 alias = "euc1"
 region = "eu-central-1"
 }
 
 resource "aws_security_group" "allow_ssh" {
+  provider    = aws.usw1
   name        = "bastion-us-west-1"
   description = "Allow SSH inbound traffic"
   vpc_id      = data.terraform_remote_state.vpc_list.outputs.admin-vpc-usw1
