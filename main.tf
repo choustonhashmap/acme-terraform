e5e67500cc3698ed9196f2aaab133494142756ee
choustonhashmap <chris.houston@hashmapinc.com>
2020-03-05 11:34:15 -0500

    security group initial add

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ output "vpc"{
 }
 
 provider "aws"{
+  alias = "usw1"
   region = "us-west-1"
 }
 provider "aws" {
@@ -31,6 +32,7 @@ region = "eu-central-1"
 }
 
 resource "aws_security_group" "allow_ssh" {
+  provider    = "usw1"
   name        = "bastion-us-west-1"
   description = "Allow SSH inbound traffic"
   vpc_id      = data.terraform_remote_state.vpc_list.outputs.admin-vpc-usw1
